a3635046c216fd161c61e580b22bc921b9a1b373
viobl <shinedakth@gmail.com>
2023-03-23 08:38:42 +0900

    031 permutation 중복순열 풀이

---
+++ pic/permutation.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+# s = input().rstrip()
+
+def DFS(L):
+    global cnt
+    if L == m:
+        for j in range(m):
+            print(res[j], end=' ')
+        print()
+        cnt += 1
+
+    else:
+        for i in range(1, n + 1):
+            res[L] = i
+            DFS(L + 1)
+
+n, m = map(int, input().split())
+res = [0] * m
+cnt = 0
+DFS(0)
+print(cnt)
