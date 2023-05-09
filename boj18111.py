16a3f6129a77cca28615de4a6fe34a55e2f26a69
viobl <shinedakth@gmail.com>
2023-05-09 15:00:10 +0900

    115 boj18111 풀이 - 마인크레프트 구현

---
+++ boj18111.py
@@ -0,0 +1,32 @@
+# 마인크레프트
+# 보유 블록 수 내에서 install 가능
+# install - uninstall 뭐가더 빠르고 적절한지 비교 제거는 2초, 설치는 1초
+# 최소값을 저장해서 활용
+import sys
+input = sys.stdin.readline
+
+n, m, b = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+res = sys.maxsize
+idx = 0
+
+# 0 ~ 256까지 반복
+for x in range(257):
+    maxS, minS = 0, 0
+
+    for i in range(n):
+        for j in range(m):
+            # 블록 >= 층
+            if graph[i][j] >= x:
+                maxS += graph[i][j] - x
+            else: # 블록 < 층
+                minS += x - graph[i][j]
+
+    if maxS + b >= minS:
+        # 최소 시간 비교
+        if minS + (maxS * 2) <= res:
+            res = minS + (maxS * 2) # 최소 시간
+            idx = x # 층 수
+
+print(res, idx)
