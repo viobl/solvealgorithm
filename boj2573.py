8b063273eb2f1420fe003afcafd269e77fd12f80
viobl <shinedakth@gmail.com>
2023-06-13 09:06:46 +0900

    121 boj2573 풀이 - bfs

---
+++ boj2573.py
@@ -0,0 +1,63 @@
+# 빙산
+# 현재 좌표 탐색하는 것 기준으로 상하좌우 탐색후 바다 접한 것
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(x, y):
+    q = deque([(x, y)])
+    visited[x][y] = 1
+    seaList = []
+
+    while q:
+        x, y = q.popleft()
+        sea = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if not graph[nx][ny]:
+                    sea += 1
+                elif graph[nx][ny] and not visited[nx][ny]: # 방문하지 않은 바다
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1 # q에 추가 후 방문처리
+
+        if sea > 0:
+            seaList.append((x, y, sea))
+
+    for x, y, sea in seaList:
+        graph[x][y] = max(0, graph[x][y] - sea)
+
+    return 1
+
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+ice = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]:
+            ice.append((i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+year = 0
+
+while ice:
+    visited = [[0] * m for _ in range(n)]
+    delList = [] # 녹아야하는 빙하
+    group = 0
+    for i, j in ice:
+        if graph[i][j] and not visited[i][j]:
+            group += bfs(i, j)
+        if graph[i][j] == 0:
+            delList.append((i, j))
+    if group > 1:
+        print(year)
+        break
+    ice = sorted(list(set(ice) - set(delList)))
+    year += 1
+
+if group < 2:
+    print(0)
